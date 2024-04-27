c59f8e9efe68e928a7f51a993fd194e3a79ef8c5
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2024-04-27 23:27:54 +0900

    画像を動かす

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-'user strict';
+'use strict';
+
+{
+    const next = document.getElementById('next');
+    const ul = document.querySelector('ul');
+
+    next.addEventListener('click', () => {
+        ul.style.transform = 'translateX(-200px)';
+    });
+}
